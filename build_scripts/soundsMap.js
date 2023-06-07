9cf41949b7b06366ae196d58079c56339a5d87b0
youngmani <94336258+youngmani@users.noreply.github.com>
2023-06-06 21:28:48 -0700

    format soundsMap

--- build_scripts/soundsMap.js
+++ build_scripts/soundsMap.js
@@ -1,161 +1,161 @@
 // map java sound names to bedrock and add additional metadata
 module.exports = {
-  "block.ancient_debris.break": {
-    name: "dig.ancient_debris",
+  'block.ancient_debris.break': {
+    name: 'dig.ancient_debris',
     pitchAdjust: 11 / 12,
   },
-  "block.bell.use": {
-    name: "block.bell.hit",
+  'block.bell.use': {
+    name: 'block.bell.hit',
   },
-  "block.chest.open": {
-    name: "random.chestopen",
+  'block.chest.open': {
+    name: 'random.chestopen',
   },
-  "block.end_portal.spawn": {
-    name: "block.end_portal.spawn",
+  'block.end_portal.spawn': {
+    name: 'block.end_portal.spawn',
   },
-  "entity.cat.ambient": {
-    name: "mob.cat.meow",
+  'entity.cat.ambient': {
+    name: 'mob.cat.meow',
   },
-  "entity.cat.stray_ambient": {
-    name: "mob.cat.straymeow",
+  'entity.cat.stray_ambient': {
+    name: 'mob.cat.straymeow',
   },
-  "entity.cow.ambient": {
-    name: "mob.cow.say",
+  'entity.cow.ambient': {
+    name: 'mob.cow.say',
   },
-  "entity.creeper.primed": {
-    name: "random.fuse",
+  'entity.creeper.primed': {
+    name: 'random.fuse',
   },
-  "entity.donkey.ambient": {
-    name: "mob.horse.donkey.idle",
+  'entity.donkey.ambient': {
+    name: 'mob.horse.donkey.idle',
   },
-  "entity.ender_dragon.death": {
-    name: "mob.enderdragon.death",
+  'entity.ender_dragon.death': {
+    name: 'mob.enderdragon.death',
   },
-  "entity.enderman.stare": {
-    name: "mob.endermen.stare",
+  'entity.enderman.stare': {
+    name: 'mob.endermen.stare',
   },
-  "entity.generic.eat": {
-    name: "random.eat",
+  'entity.generic.eat': {
+    name: 'random.eat',
   },
-  "entity.generic.explode": {
-    name: "random.explode",
+  'entity.generic.explode': {
+    name: 'random.explode',
     pitchAdjust: 2 / 3,
   },
-  "entity.ghast.warn": {
-    name: "mob.ghast.charge",
+  'entity.ghast.warn': {
+    name: 'mob.ghast.charge',
   },
-  "entity.item.break": {
-    name: "random.break",
+  'entity.item.break': {
+    name: 'random.break',
   },
-  "entity.ocelot.ambient": {
-    name: "mob.ocelot.idle",
+  'entity.ocelot.ambient': {
+    name: 'mob.ocelot.idle',
   },
-  "entity.pig.ambient": {
-    name: "mob.pig.say",
+  'entity.pig.ambient': {
+    name: 'mob.pig.say',
   },
-  "entity.piglin.ambient": {
-    name: "mob.piglin.ambient",
+  'entity.piglin.ambient': {
+    name: 'mob.piglin.ambient',
   },
-  "entity.pillager.ambient": {
-    name: "mob.pillager.idle",
+  'entity.pillager.ambient': {
+    name: 'mob.pillager.idle',
   },
-  "entity.pillager.hurt": {
-    name: "mob.pillager.hurt",
+  'entity.pillager.hurt': {
+    name: 'mob.pillager.hurt',
   },
-  "entity.player.burp": {
-    name: "random.burp",
+  'entity.player.burp': {
+    name: 'random.burp',
   },
-  "entity.player.death": {
-    name: "game.player.die",
+  'entity.player.death': {
+    name: 'game.player.die',
   },
-  "entity.player.hurt": {
-    name: "game.player.hurt",
+  'entity.player.hurt': {
+    name: 'game.player.hurt',
   },
-  "entity.player.hurt_drown": {
-    name: "mob.player.hurt_drown",
+  'entity.player.hurt_drown': {
+    name: 'mob.player.hurt_drown',
   },
-  "entity.player.hurt_freeze": {
-    name: "mob.player.hurt_freeze",
+  'entity.player.hurt_freeze': {
+    name: 'mob.player.hurt_freeze',
   },
-  "entity.player.hurt_on_fire": {
-    name: "mob.player.hurt_on_fire",
+  'entity.player.hurt_on_fire': {
+    name: 'mob.player.hurt_on_fire',
   },
-  "entity.player.hurt_sweet_berry_bush": {
-    name: "block.sweet_berry_bush.hurt",
+  'entity.player.hurt_sweet_berry_bush': {
+    name: 'block.sweet_berry_bush.hurt',
   },
-  "entity.sheep.ambient": {
-    name: "mob.sheep.say",
+  'entity.sheep.ambient': {
+    name: 'mob.sheep.say',
   },
-  "entity.shulker.ambient": {
-    name: "mob.shulker.ambient",
+  'entity.shulker.ambient': {
+    name: 'mob.shulker.ambient',
   },
-  "entity.skeleton.ambient": {
-    name: "mob.skeleton.say",
+  'entity.skeleton.ambient': {
+    name: 'mob.skeleton.say',
   },
-  "entity.skeleton.death": {
-    name: "mob.skeleton.death",
+  'entity.skeleton.death': {
+    name: 'mob.skeleton.death',
   },
-  "entity.spider.ambient": {
-    name: "mob.spider.say",
+  'entity.spider.ambient': {
+    name: 'mob.spider.say',
   },
-  "entity.spider.death": {
-    name: "mob.spider.death",
+  'entity.spider.death': {
+    name: 'mob.spider.death',
   },
-  "entity.stray.ambient": {
-    name: "mob.stray.ambient",
+  'entity.stray.ambient': {
+    name: 'mob.stray.ambient',
   },
-  "entity.stray.death": {
-    name: "mob.stray.death",
+  'entity.stray.death': {
+    name: 'mob.stray.death',
   },
-  "entity.villager.ambient": {
-    name: "mob.villager.idle",
+  'entity.villager.ambient': {
+    name: 'mob.villager.idle',
   },
-  "entity.villager.death": {
-    name: "mob.villager.death",
+  'entity.villager.death': {
+    name: 'mob.villager.death',
   },
-  "entity.villager.hurt": {
-    name: "mob.villager.hit",
+  'entity.villager.hurt': {
+    name: 'mob.villager.hit',
   },
-  "entity.villager.no": {
-    name: "mob.villager.no",
+  'entity.villager.no': {
+    name: 'mob.villager.no',
   },
-  "entity.villager.trade": {
-    name: "mob.villager.haggle",
+  'entity.villager.trade': {
+    name: 'mob.villager.haggle',
   },
-  "entity.villager.yes": {
-    name: "mob.villager.yes",
+  'entity.villager.yes': {
+    name: 'mob.villager.yes',
   },
-  "entity.wandering_trader.ambient": {
-    name: "mob.wanderingtrader.idle",
+  'entity.wandering_trader.ambient': {
+    name: 'mob.wanderingtrader.idle',
   },
-  "entity.wandering_trader.death": {
-    name: "mob.wanderingtrader.death",
+  'entity.wandering_trader.death': {
+    name: 'mob.wanderingtrader.death',
   },
-  "entity.wandering_trader.hurt": {
-    name: "mob.wanderingtrader.hurt",
+  'entity.wandering_trader.hurt': {
+    name: 'mob.wanderingtrader.hurt',
   },
-  "entity.wandering_trader.no": {
-    name: "mob.wanderingtrader.no",
+  'entity.wandering_trader.no': {
+    name: 'mob.wanderingtrader.no',
   },
-  "entity.wandering_trader.trade": {
-    name: "mob.wanderingtrader.haggle",
+  'entity.wandering_trader.trade': {
+    name: 'mob.wanderingtrader.haggle',
   },
-  "entity.wandering_trader.yes": {
-    name: "mob.wanderingtrader.yes",
+  'entity.wandering_trader.yes': {
+    name: 'mob.wanderingtrader.yes',
   },
-  "entity.witch.ambient": {
-    name: "mob.witch.ambient",
+  'entity.witch.ambient': {
+    name: 'mob.witch.ambient',
   },
-  "entity.zombie.ambient": {
-    name: "mob.zombie.say",
+  'entity.zombie.ambient': {
+    name: 'mob.zombie.say',
   },
-  "entity.zombie.hurt": {
-    name: "mob.zombie.hurt",
+  'entity.zombie.hurt': {
+    name: 'mob.zombie.hurt',
   },
-  "entity.zombified_piglin.hurt": {
-    name: "mob.zombiepig.zpighurt",
+  'entity.zombified_piglin.hurt': {
+    name: 'mob.zombiepig.zpighurt',
   },
-  "item.axe.strip": {
-    name: "use.wood",
+  'item.axe.strip': {
+    name: 'use.wood',
   },
 };
