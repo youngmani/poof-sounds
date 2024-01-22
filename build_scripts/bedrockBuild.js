2d47278a86afc1bdc11d91a7ec4f0ce9291cb744
youngmani <94336258+youngmani@users.noreply.github.com>
2024-01-22 00:02:41 -0800

    remove subtitles from bedrock

--- build_scripts/bedrockBuild.js
+++ build_scripts/bedrockBuild.js
@@ -117,6 +117,7 @@ const generateSoundDefinitions = soundsJson => {
       }
       const sound = { ...javaSounds[key], ...propOverrides };
       delete sound.replace;
+      delete sound.subtitle;
       sound.sounds = adjustSounds(sound.sounds, adjustments);
       definitions[name] = sound;
       log.debug(`converting ${key} to ${name}`);
